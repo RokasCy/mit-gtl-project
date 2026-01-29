9366f6123633ba913d06fd0f39ad41d168e78e69
RokasCy <rokas.rukuiza@gmail.com>
2026-01-29 10:15:13 +0200

    better detection

--- src/blank_package/blank_package/tape_detect.py
+++ src/blank_package/blank_package/tape_detect.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
 
-def get_postion(cx, cy, bh, bw):
+def get_postion(cx, cy, frame):
+    imgh, imgw = frame.shape[0], frame.shape[1]
+    bw = imgw // 3
+    bh = imgh // 3
+
     position = ""
     if cy < bh:
         position += "top "
@@ -19,53 +23,67 @@ def get_postion(cx, cy, bh, bw):
 
     return position
 
-def detect_parking(frame):
-    img_width, img_height = frame.shape[0], frame.shape[1]
-    buffer_width = img_width // 3
-    buffer_height = img_height // 3
+def draw_borders(frame):
+    imgh, imgw = frame.shape[0], frame.shape[1]
+    bw = imgw // 3
+    bh = imgh // 3
 
+    cv2.line(frame, (bw, 0), (bw, imgh), (0, 0, 255), 2)
+    cv2.line(frame, (bw*2, 0), (bw*2, imgh), (0, 0, 255), 2)
+
+    cv2.line(frame, (0, bh), (imgw, bh), (0, 0, 255), 2)
+    cv2.line(frame, (0, bh*2), (imgw, bh*2), (0, 0, 255), 2)
+
+def detect_parking(frame):
     # Hue Saturation Value
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #getting red
-    lower_red = np.array([0, 120, 100], np.uint8)
-    upper_red = np.array([5, 255, 255], np.uint8)
-    mask = cv2.inRange(hsv, lower_red, upper_red)
 
+    #getting red
+    lower_red = np.array([0, 100, 150], np.uint8)
+    upper_red = np.array([5, 255, 255], np.uint8)
+
+    lower_red2 = np.array([170, 100, 150], np.uint8)
+    upper_red2 = np.array([179, 255, 255], np.uint8)
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+
+    red_mask = cv2.bitwise_or(mask, mask2)
 
     #removing noise
-    kernel = np.ones((10, 10), np.uint8)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    kernel = np.ones((5, 5), np.uint8)
+    red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel)
+    red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_CLOSE, kernel)
 
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    max_cnt, max_area = None, 0
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
+    draw_borders(frame)
 
-        if area > max_area and area > 500:
-            max_area = area
-            max_cnt = cnt
+    #finding contours
+    contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    max_cnt = max(contours, key=cv2.contourArea, default=None)
 
-    if max_cnt is None:
-        return False
-
+    if max_cnt is None or cv2.contourArea(max_cnt) < 200:
+        return (False, 0, 0)
 
     x, y, w, h = cv2.boundingRect(max_cnt)
     cx = x + w // 2
     cy = y + h // 2
 
-    print(get_postion(cx, cy, buffer_width, buffer_height))
+    print(get_postion(cx, cy, frame))
     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.imshow("mask", mask)
-    #print(f"coordinates: {(cx, cy)}")
 
-    return True
+    return (True, cx, cy)
 
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    detect_parking(frame)
-    cv2.imshow("frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-q
+
+'''def detect_road(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([17, 255, 50])
+
+    black_mask = cv2.inRange(hsv, lower_black, upper_black)
+
+    kernel = np.ones((5, 5), np.uint8)
+    black_mask = cv2.morphologyEx(black_mask, cv2.MORPH_OPEN, kernel)
+    black_mask = cv2.morphologyEx(black_mask, cv2.MORPH_CLOSE, kernel)
+
+'''
