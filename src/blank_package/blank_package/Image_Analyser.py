b956e6b4c27854d8501dbf8258e2287d20ef628a
Good-Life-Always <tselishchevam4@gmail.com>
2026-01-30 09:02:21 +0200

    changed main py file to third version of code

--- src/blank_package/blank_package/Image_Analyser.py
+++ src/blank_package/blank_package/Image_Analyser.py
@@ -14,7 +14,6 @@ from geometry_msgs.msg import Twist
 from rclpy.time import Duration
 
 
-
 class ImageSaver(Node):
     def __init__(self):
         super().__init__('image_saver')
@@ -29,7 +28,7 @@ class ImageSaver(Node):
         self.timer_led = self.create_timer(0.1, self.publish_detection)
 
         self.wheels_pub = self.create_publisher(WheelsCmdStamped, f'/{self.vehicle_name}/wheels_cmd', 10)
-        #self.timer_rotate  = self.create_timer(0.1, self.rotate)
+        # self.timer_rotate  = self.create_timer(0.1, self.rotate)
 
         self.spotted_parking = False
         self.obj_x, self.obj_y = 0, 0
@@ -38,59 +37,67 @@ class ImageSaver(Node):
 
         self.counter = 0
 
+        self.action_timer = None
+        self.action_end_time = None
+
     def save_image(self, msg):
         if self.counter % 15 != 0:
             self.counter += 1
             return
 
-        #convert to 1D numpy array
+        # convert to 1D numpy array
         np_arr = np.frombuffer(msg.data, np.uint8)
-        #convert into BGR
+        # convert into BGR
         frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         self.frame = frame
 
         self.spotted_parking, self.obj_x, self.obj_y = tape_detect.detect_parking(frame, self.output_dir, self.counter)
         self.counter += 1
 
-        self.get_clock().sleep_for(Duration(seconds=0.5))
+        # self.get_clock().sleep_for(Duration(seconds=0.5))
 
-        #test part
+        # test part
+
+    '''        if self.frame is None:
+            return
+
+        if not self.spotted_parking:
+            self.turn_left(1)
+        elif self.spotted_parking:
+
+            frame_center_x = self.frame.shape[1] // 2
+            frame_center_y = self.frame.shape[0] // 2
+
+            error = self.obj_x - frame_center_x
+            rotation_speed = 0
+
+            dead_zone = 50  # pixels
+            if abs(error) < dead_zone:
+                self.move_forward()
+            elif error > 0:
+                self.turn_right(0.1)
+            elif error < 0:
+                self.turn_left(0.1)'''
+
+    def process_movement(self):
+
         if self.frame is None:
             return
 
-        frame_center_x = self.frame.shape[1] // 2
-        frame_center_y = self.frame.shape[0] // 2
-
-        error = self.obj_x - frame_center_x
-        rotation_speed = 0
-
-        dead_zone = 50  # pixels
-        if abs(error) < dead_zone:
-            self.move_forward()
-        elif error > 0:
-            self.turn_right(0.1)
-        elif error < 0:
+        if not self.spotted_parking:
             self.turn_left(0.1)
+        else:
 
-    '''def rotate(self):
-        if self.frame is None:
-            return 
-        
-        frame_center_x = self.frame.shape[1] // 2
-        frame_center_y = self.frame.shape[0] // 2
+            frame_center_x = self.frame.shape[1] // 2
+            error = self.obj_x - frame_center_x
+            dead_zone = 50  # pixels
 
-        error = self.obj_x - frame_center_x
-        rotation_speed = 0
-
-        dead_zone = 10  # pixels
-        if abs(error) < dead_zone:
-            self.move_forward()
-        elif error > 0:
-            self.turn_right(1)
-        elif error < 0:
-            self.turn_left(0.3)'''
-        
-
+            if abs(error) < dead_zone:
+                self.move_forward()
+            elif error > 0:
+                self.turn_right(0.1)
+            else:
+                self.turn_left(0.1)
 
     def run_wheels(self, frame_id, vel_left, vel_right):
         wheel_msg = WheelsCmdStamped()
@@ -102,29 +109,38 @@ class ImageSaver(Node):
         wheel_msg.vel_right = vel_right
         self.wheels_pub.publish(wheel_msg)
 
-    def turn_left(self,speed):
+    def turn_left(self, speed):
         self.get_logger().info("Turning left")
-        self.run_wheels('right_callback', 0.0, speed)
-        self.get_clock().sleep_for(Duration(seconds=1))
-        self.run_wheels('stop_callback', 0.0, 0.0)
+        self.run_wheels('left_callback', 0.0, speed)
+        # Schedule stop after 1 second without blocking
+        self.action_end_time = self.get_clock().now() + Duration(seconds=1)
+        if self.action_timer is None:
+            self.action_timer = self.create_timer(0.05, self.check_action_complete)
 
-
-    def turn_right(self,speed):
+    def turn_right(self, speed):
         self.get_logger().info("Turning right")
         self.run_wheels('right_callback', speed, 0.0)
-        self.get_clock().sleep_for(Duration(seconds=1))
-        self.run_wheels('stop_callback', 0.0, 0.0)
+        self.action_end_time = self.get_clock().now() + Duration(seconds=1)
+        if self.action_timer is None:
+            self.action_timer = self.create_timer(0.05, self.check_action_complete)
+
     def move_forward(self):
         self.get_logger().info("Moving forward")
         self.run_wheels('forward_callback', 0.4, 0.4)
-        self.get_clock().sleep_for(Duration(seconds=1))
-        self.run_wheels('stop_callback', 0.0, 0.0)
+        self.action_end_time = self.get_clock().now() + Duration(seconds=1)
+        if self.action_timer is None:
+            self.action_timer = self.create_timer(0.05, self.check_action_complete)
+
+    def check_action_complete(self):
+        if self.action_end_time and self.get_clock().now() >= self.action_end_time:
+            self.stop_movement()
+            self.action_timer.destroy()
+            self.action_timer = None
+            self.action_end_time = None
 
     def stop_movement(self):
         self.get_logger().info("Stopping movement")
         self.run_wheels('stop_callback', 0.0, 0.0)
-
-
 
     def publish_detection(self):
         # LEDPattern is a custom Duckietown Message
@@ -134,18 +150,19 @@ class ImageSaver(Node):
             pattern = ColorRGBA(r=0.0, g=1.0, b=0.0, a=1.0)
         else:
             pattern = ColorRGBA(r=1.0, g=0.0, b=0.0, a=1.0)
-        
+
         # 5 Leds to fill
         msg.rgb_vals = [pattern] * 5
 
         self.publisher_led.publish(msg)
-    
+
 
 def main():
     rclpy.init()
     image_saver = ImageSaver()
     rclpy.spin(image_saver)
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
